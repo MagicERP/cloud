c7e446a01d6e9d55fd7174f0b01d0591ae749e30
ray.wang <bookast@qq.com>
2014-07-15 08:04:13 +0800

    cloud

---
+++ core/src/main/java/com/magicerp/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.magicerp.service.impl;
+
+import com.magicerp.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
